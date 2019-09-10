183f5f3553ea6131150ceff5be322dd07e88958c
Your Name <mohnad9@gmail.com>
2019-09-10 12:58:34 +0300

    first commit

---
+++ Assets/angry.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angry : MonoBehaviour
+{
+    bool isClicked = false;
+    public Rigidbody2D rg;
+    public SpringJoint2D sj;
+
+    void Update()
+    {
+        if (isClicked)
+        {
+            rg.position =Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    private void OnMouseDown()
+    {
+        isClicked = true;
+        rg.isKinematic = true;
+    }
+    private void OnMouseUp()
+    {
+        isClicked = false;
+        rg.isKinematic = false;
+
+        StartCoroutine(wait());
+    
+    }
+    IEnumerator wait()
+    {
+        yield return new WaitForSecondsRealtime(0.15f);
+        sj.enabled = false;
+    }
+    
+ 
+
+}
---
+++ Assets/smile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smile : MonoBehaviour
+{
+    private float hit = 4;
+    public GameObject effect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude > hit)
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        
+    }
+}
